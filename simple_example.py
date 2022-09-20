bad06d2ab4e87a40b17d21214fb1cd2ce5dab6e6
Honglu Fan <64070721+honglu2875@users.noreply.github.com>
2022-09-20 09:02:39 -0600

    Add files via upload

---
+++ simple_example.py
@@ -0,0 +1,90 @@
+import torch
+from torch import nn
+from torch import optim
+
+def g1(x, y):
+    return x * y
+
+def g2(x, y):
+    return x * x
+
+def g3(x, y):
+    return y * y
+
+
+class SmallNet(nn.Module):
+    def __init__(self, input_dim, fns):
+        super().__init__()
+        self.input_dim = input_dim
+        self.fns = fns
+        self.lin1 = nn.Linear(len(self.fns), len(self.fns), bias=True)
+        self.output = nn.Linear(len(self.fns), 1, bias=True)
+
+    def forward(self, x):
+        out = []
+        inp_as_args = [x[:, i:i+1] for i in range(self.input_dim)]
+        for f in self.fns:
+            out.append(f(*inp_as_args))
+        x = torch.cat(out, 1)  # Pass through the given functions
+        y = self.lin1(x)  
+        y = y * y  # Activation function 
+        out = self.output(y)
+        return out
+
+
+def recursive_reg(x):
+    if isinstance(x, dict):
+        s = 0
+        for value in x.values():
+            s += torch.sum(torch.abs(recursive_reg(value)))
+        return s
+    return torch.sum(torch.abs(x))
+
+def main():
+    epochs = 10000
+    input_dim = 2
+    sample_size = 100
+    fns = (g1, g2, g3)
+
+    net_ground_truth = SmallNet(input_dim, fns)
+   
+    """ 
+    # The ground truth network for testing purpose
+    net_ground_truth = SmallNet(input_dim, fns)
+    state_dict = net_ground_truth.state_dict()
+    state_dict['lin1.weight'] = torch.tensor([[1, 0, 0], [0, 1, 1], [0, 1, -1]]) + torch.rand((3,3)) * 0.5
+    state_dict['lin1.bias'] = torch.zeros((3,))
+    state_dict['output.weight'] = torch.tensor([[1, -0.25, 0.25]])
+    state_dict['output.bias'] = torch.tensor([0])
+    #state_dict['output.bias'] = torch.tensor([0])
+    net_ground_truth.load_state_dict(state_dict)
+    """
+
+    opt = optim.Adam(net_ground_truth.parameters(), lr=0.001)
+    loss_fn = nn.functional.mse_loss
+    zero = torch.zeros((sample_size, 1))
+    for i in range(epochs):
+        inp = torch.rand((sample_size, input_dim)) * 1
+        opt.lr=0.001/(i+1)
+        # Training
+        opt.zero_grad()
+        output = net_ground_truth(inp)
+        loss = loss_fn(output, zero) + recursive_reg(net_ground_truth.state_dict())
+        #loss = loss_fn(output, zero) 
+        #loss = 
+        #loss.backward()
+        #opt.step()
+
+        print(loss)
+
+    for i in range(10):
+        inp = torch.rand((sample_size, input_dim)) * 10
+        #print(net_ground_truth(inp))
+        print(loss_fn(net_ground_truth(inp), zero))
+    
+    print(net_ground_truth.state_dict())
+
+
+if __name__ == '__main__':
+    main()
+
